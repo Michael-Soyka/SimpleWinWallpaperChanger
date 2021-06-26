5c0d78a2d5b277863b7e87c3bf18014ef10dd44d
Devafler2019 <45078459+Michael-Soyka@users.noreply.github.com>
2021-06-26 16:33:06 +0300

    Update changewallpaper.py

--- changewallpaper.py
+++ changewallpaper.py
@@ -4,7 +4,5 @@ import argparse
 parser = argparse.ArgumentParser(add_help=True, prog='WallpaperChanger', description=' Change you wallpaper | By Michael Soyka ')
 parser.add_argument('path',action="store", help='Path to image')
 
-wppath = parser.parse_args().path
-
-ctypes.windll.user32.SystemParametersInfoW(20, 0, wppath, 3)
+ctypes.windll.user32.SystemParametersInfoW(20, 0, parser.parse_args().path, 3)
 
