1f4e702b980b0bce424bd9ae009a81209bf8dc48
Devafler2019 <45078459+Michael-Soyka@users.noreply.github.com>
2022-05-20 10:34:44 +0300

    Added platform check.

--- changewallpaper.py
+++ changewallpaper.py
@@ -1,8 +1,11 @@
 import ctypes
 import argparse
+from sys import platform
 
 parser = argparse.ArgumentParser(add_help=True, prog='WallpaperChanger', description=' Change you wallpaper | By Michael Soyka ')
 parser.add_argument('path',action="store", help='Path to image')
 
-ctypes.windll.user32.SystemParametersInfoW(20, 0, parser.parse_args().path, 3)
-
+if platform == "win32":
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, parser.parse_args().path, 3)
+else:
+    print("Sorry, but this script only works under windows")
